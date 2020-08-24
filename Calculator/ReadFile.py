00d582740bf8bf3958c6b4bd6e856dc2aa200ebe
AndaChain <32815766+AndaChain@users.noreply.github.com>
2020-08-24 23:42:04 +0700

    Create ReadFile.py

---
+++ Calculator/ReadFile.py
@@ -0,0 +1,12 @@
+class Read:
+    def __init__(self,path):
+        self.files = open(path, "r")
+        self.path = path
+    
+    def getInput(self):
+        output = self.files.read()
+        return output.split("\n")
+
+if __name__ == "__main__":
+    temp = Read("C:\\Users\\s6201012620279\\Software development\\Calculator\\Input.txt")
+    print(temp.getInput())
